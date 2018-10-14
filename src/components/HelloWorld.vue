2b13420b1d7c74c2ff8f8ae516c13da0080e17ba
Katsunori Takahashi <katsunori.t10@gmail.com>
2018-10-14 17:33:45 +0900

    [add] base width field

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,13 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+    <v-layout row>
+      <v-text-field
+        type="number"
+        label="Base Width"
+        value="320"
+        suffix="px"
+      ></v-text-field>
+    </v-layout>
   </v-container>
 </template>
 
