435cebdf5e609c50cd3a9e6f89b583e45d0121b8
Katsunori Takahashi <katsunori.t10@gmail.com>
2018-11-11 16:03:49 +0900

    feat: calc vw

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,69 @@
     <v-toolbar app>
       <v-toolbar-title v-text="title"></v-toolbar-title>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container grid-list-md>
+        <v-layout row>
+          <v-flex xs6>
+            <v-select v-model.number="baseWidth" :items="devices" label="Device"></v-select>
+          </v-flex>
+          <v-flex xs6>
+            <v-text-field v-model.number="baseWidth" type="number" label="Base Width" suffix="px"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs6>
+            <v-text-field v-model.number="fontSizePx" type="number" label="Font Size (px)" value="10" suffix="px"></v-text-field>
+          </v-flex>
+          <v-flex xs6>
+            <v-text-field type="text" label="Font Size (vw)" :value="calcVw" suffix="vw" readonly></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex>
+            <v-textarea label="フォントサイズが画面幅に応じて拡縮します" value="test" :style="{fontSize: `${calcVw}vw`}"></v-textarea>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
-
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+    <v-footer app>
+      <v-text class="ml-2">&copy; 2018</v-text>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return ({
+      title: 'px-vw-calclator',
+      selectedDevice: null,
+      devices: [
+        {
+          text: "iPhone5/SE",
+          value: 320,
+        },
+        {
+          text: "iPhone6/7/8/X",
+          value: 375,
+        },
+        {
+          text: "iPhone6/7/8 Plus",
+          value: 414,
+        },
+        {
+          text: "Responsive",
+          value: "",
+        },
+      ],
+      baseWidth: 320,
+      fontSizePx: 10,
+    });
   },
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'px-vw-rem-calcrator'
+  computed: {
+    calcVw() {
+      return (100 / this.baseWidth) * this.fontSizePx;
     }
   }
 }
